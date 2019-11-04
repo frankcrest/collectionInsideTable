26aff309c0876ff21a200d076829f065eb6b2e68
frankcrest <frankcrestview@hotmail.com>
2019-11-03 21:15:07 -0500

    Initial Commit

---
+++ CollectionViewExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CollectionViewExample
+//
+//  Created by Frank Chen on 2019-11-03.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
